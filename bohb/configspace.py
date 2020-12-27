91c079b639ea7015c8d9bfc01e1e0f0f405ea820
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2020-12-27 14:11:16 +0300

    configspace

---
+++ bohb/configspace.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+
+class Configuration:
+    def __init__(self, hyperparameters):
+        self.hyperparameters = hyperparameters
+        self.max_length = max(
+            [len(hyperparameter.name) for hyperparameter in self.hyperparameters])
+
+    def to_dict(self):
+        config = {}
+        for hyperparameter in self.hyperparameters:
+            config[hyperparameter.name] = hyperparameter.value['value']
+        return config
+
+    def __str__(self):
+        string = ["Configuration:\n"]
+        for hyperparameter in self.hyperparameters:
+            string.append((f'{"Name:":>8} {hyperparameter.name: <{self.max_length}}, '
+                           f"Value: {hyperparameter.value['value']}\n").ljust(10))
+        return ''.join(string)
+
+
+class Hyperparameter:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+
+class ConfigurationSpace:
+    def __init__(self, hyperparameters, seed=None):
+        self.hyperparameters = hyperparameters
+        self.rng = np.random.default_rng(seed)
+
+    def sample_configuration(self):
+        hyperparameters = []
+        for hyperparameter in self.hyperparameters:
+            value = hyperparameter.sample(self.rng)
+            hyperparameters.append(
+                Hyperparameter(hyperparameter.name, value))
+        return Configuration(hyperparameters)
+
+
+class UniformHyperparameter(Hyperparameter):
+    def __init__(self, name, lower, upper, integer=False):
+        value = (lower + upper) / 2
+        value = int(value) if integer else value
+        super().__init__(name, value)
+        self.lower = lower
+        self.upper = upper
+        self.integer = integer
+
+    def sample(self, rng):
+        func = rng.integers if self.integer else rng.uniform
+        value = func(self.lower, self.upper)
+        return {'index': -1, 'value': value}
+
+
+class CategoricalHyperparameter(Hyperparameter):
+    def __init__(self, name, choices):
+        super().__init__(name, choices[0])
+        self.choices = choices
+
+    def sample(self, rng):
+        index = rng.integers(0, len(self.choices))
+        return {'index': index, 'value': self.choices[index]}
+
+a = UniformHyperparameter('hidden_size', 0, 100, True)
+b = UniformHyperparameter('hidden', 0, 10, True)
+cs = ConfigurationSpace([a, b], 123)
+config = cs.sample_configuration()
+print(config.hyperparameters[0].value['index'])
